639a4267e14b3d2628da02cd88f984c4da89deee
Chris McGinn <chris.s.mcginn@live.com>
2018-06-15 14:34:25 -0400

    Project Creation

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PulseConsole.Services;
+
+namespace PulseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RunMain();
+
+        }
+        /// <summary>
+        /// Allows for continuation of program until user decides to exit
+        /// </summary>
+        static void RunMain()
+        {
+            var maxTries = 10;
+            var tries = 0;
+            var inputValue = default(int?);
+
+            while (!inputValue.HasValue && tries < 10)
+            {
+                tries += 1;
+                inputValue = GetInputAction();
+                if (!inputValue.HasValue)
+                    Console.Out.WriteLine("Input Value was not Recognized as a Valid Integer");
+            }
+
+            if (inputValue.GetValueOrDefault() == 0)
+                Environment.Exit(0);
+            else
+            {
+                var records = MemberService.LookupMembershipInfo(inputValue.GetValueOrDefault());
+                var output = MemberService.GetMembershipInfoResponse(records);
+                output.ForEach(o => Console.Out.WriteLine(o));
+                RunMain();
+            }
+
+        }
+        /// <summary>
+        /// Handles user input
+        /// </summary>
+        /// <returns></returns>
+        static int? GetInputAction()
+        {
+            int? result = default(int?);
+            Console.Out.WriteLine("Please Enter a MemberID to Query Results or Enter 0 to Quit Application");
+            var input = Console.In.ReadLine();
+            var inputTest = 0;
+            if (int.TryParse(input, out inputTest))
+                result = inputTest;
+            return result;
+        }
+    }
+}
---
+++ Services/MemberService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseConsole.Services
+{
+    public class MemberService
+    {
+        public class MembershipInfo
+        {
+            public int MemberID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public int? MostSevereDiagnosisID { get; set; }
+
+            public string MostSevereDiagnosticDescription { get; set; }
+
+            public int? CategoryID { get; set; }
+
+            public string CategoryDescription { get; set; }
+
+            public int? CategoryScore { get; set; }
+
+            public int IsMostSevereCategory { get; set; }
+
+        }
+        /// <summary>
+        /// Looks up MemberDiagnosisInfoView based on Member ID
+        /// Old School SQL Query because we only have a single table, and one query that is always the same
+        /// </summary>
+        /// <param name="memberId"></param>
+        /// <returns></returns>
+        public static List<MembershipInfo> LookupMembershipInfo(int memberId)
+        {
+            var result = new List<MembershipInfo>();
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("SELECT [MemberID] ,[FirstName] ,[LastName] ,[MostSevereDiagnosisID] ,[MostSevereDiagnosticDescription] ,[CategoryID] ,[CategoryDescription] ,[CategoryScore] ,[IsMostSevereCategory] FROM MemberDiagnosisInfoView WHERE MEMBERID = @MemberId", con);
+                cmd.Parameters.AddWithValue("@MemberId",memberId);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var r = new MembershipInfo();
+                    r.MemberID = rdr.GetInt32(0);
+                    r.FirstName = rdr.GetString(1);
+                    r.LastName = rdr.GetString(2);
+                    if (rdr[3] != null)
+                        r.MostSevereDiagnosisID = rdr.GetInt32(3);
+                    r.MostSevereDiagnosticDescription = rdr.GetString(4);
+                    if (rdr[5] != null)
+                        r.CategoryID = rdr.GetInt32(5);
+                    r.CategoryDescription = rdr.GetString(6);
+                    if (rdr[7] != null)
+                        r.CategoryScore = rdr.GetInt32(7);
+                    r.IsMostSevereCategory = rdr.GetInt32(8);
+                    result.Add(r);
+                   
+                 
+                }
+                con.Close();
+            }
+
+            return result;
+        }
+        /// <summary>
+        /// Provides response as list of strings to support console output
+        /// </summary>
+        /// <param name="membershipInfos"></param>
+        /// <returns></returns>
+        public static List<string> GetMembershipInfoResponse(List<MembershipInfo> membershipInfos)
+        {
+            var result = new List<string>();
+            var header =
+                "MemberID|FirstName|LastName|MostSevereDiagnosisID|MostSevereDiagnosticDescription|CategoryID|CategoryDescription|CategoryScore|IsMostSevereCategory";
+            result.Add(header);
+            result.Add("".PadLeft(header.Count(), '-'));
+            var recordValues = membershipInfos.Select(mi => String.Format("{0}|{1}|{2}|{3}|{4}|{5}|{6}|{7}|{8}",
+                mi.MemberID, mi.FirstName,
+                mi.LastName, mi.MostSevereDiagnosisID, mi.MostSevereDiagnosticDescription, mi.CategoryID,
+                mi.CategoryDescription, mi.CategoryScore, mi.IsMostSevereCategory)).ToList();
+            recordValues.ForEach(ri =>
+            {
+                result.Add(ri);
+                result.Add("".PadLeft(ri.Count(), '-'));
+            });
+
+            result.Add(String.Format("{0} Records Found", membershipInfos.Count));
+            return result;
+
+        }
+    }
+}
